d2d6235eca9d2ff637d573d2acf538b8cc6cd064
Radim Goliáš <ragoli66@gmail.com>
2024-09-17 13:04:44 +0200

    dynmap controls next test

--- BlazorServer/wwwroot/scripts.js
+++ BlazorServer/wwwroot/scripts.js
@@ -6,18 +6,9 @@ function blazorGetTimezoneOffset() {
     return new Date().getTimezoneOffset();
 }
 
-function dynmapRemoveControls() {
-    var iframe = document.getElementById("iframe1");
-    var elmnt = iframe.contentWindow.document.getElementsByClassName("control-bar")[0];
-    var elmnt = iframe.contentWindow.document.getElementsByClassName("control-bar")[1];
-    var elmnt = iframe.contentWindow.document.getElementsByClassName("control-bar")[2];
-    elmnt.style.display = "none";
-    elmnt.style.visibility = "hidden";
-}
-
-function dynmapAppendCss() {
+function dynmapAppendCssRemoveControls() {
     var cssLink = document.createElement("link");
-    cssLink.href = "dynmap-custom.css";
+    cssLink.href = "dynmap-removecontrols.css";
     cssLink.rel = "stylesheet";
     cssLink.type = "text/css";
     frames['iframe1'].document.head.appendChild(cssLink);
